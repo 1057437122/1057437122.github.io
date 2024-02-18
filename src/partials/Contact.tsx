452156efb54c006d8f1488878fe5d1c1f089d1b9
baishun <libaishun007@gmail.com>
2024-02-18 09:58:53 +0100

    feat: change footer; add mailto method

--- src/partials/Contact.tsx
+++ src/partials/Contact.tsx
@@ -2,9 +2,13 @@ import { GradientText, Section } from '@/dependences';
 
 const Contact = () => (
   <Section>
-    <div>
-      If you have any questions on me, send me a email:{' '}
-      <GradientText>lecy.cc.app@gmail.com </GradientText>
+    <div className="bg-slate-800 p-7">
+      <div className="flex items-center">
+        For business cooperation, please send email to :{' '}
+        <GradientText>
+          <a href="mailto:lecy.cc.app@gmail.com">lecy.cc.app@gmail.com</a>{' '}
+        </GradientText>
+      </div>
     </div>
   </Section>
 );
