eb908463b7877db2dbfa4bc722f81a6c805051f1
Felix <>
2024-02-19 11:07:27 +0100

    feat: change avatar

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -27,8 +27,8 @@ const Hero = () => (
       }
       avatar={
         <img
-          className="h-80 w-64"
-          src="/assets/images/avatar.svg"
+          className="w-70 h-80"
+          src="/assets/images/hacker.png"
           alt="Avatar image"
           loading="lazy"
         />
